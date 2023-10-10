4b0370a93ad8a4c480466d3ca22acb08666fe0b4
Andy <me@something.com>
2023-10-10 10:51:34 -0500

    Actually filter FC edges if filter present. Add model inversion

--- +mlnla/MLCrossVal.m
+++ +mlnla/MLCrossVal.m
@@ -29,6 +29,9 @@ classdef MLCrossVal
 
             edgeIdxsToKeep = obj.featureFilter.filter(fcTrain, behavTrain);
             output.fcEdgesKept = edgeIdxsToKeep;
+            fcTrain = fcTrain(:,edgeIdxsToKeep);
+            fcTest = fcTest(:,edgeIdxsToKeep);            
+            numFeatures = length(edgeIdxsToKeep);
 
             %This line should be flexible to accomodate lasso,
             %fitrlinear
@@ -47,37 +50,48 @@ classdef MLCrossVal
             output.CI_SVR_linear = [CI_left_SVR_linear(1,2),CI_right_SVR_linear(1,2)];
             output.R2_SVR_linear = (corr_SVR_linear(1,2))^2;
             
+            % New interpretation
+            behav_train_fit = predict(fitSVRLinear,fcTrain','ObservationsIn','columns');
+            inv_coef = zeros(numFeatures,1);
+            for m = 1:numFeatures
+                cov_mat = cov(behav_train_fit,fcTrain(:,m));
+                inv_coef(m) = cov_mat(2);
+            end
+            output.behav_train_fit = behav_train_fit;
+            output.inv_coef = inv_coef;
+            
             
         end
         
         function [outputAllReps, avgOutput] = executeNTimes(obj, fcData, behavior, groupIds, numRepetitions)
             
-            MSE_SVR_linear = zeros(numRepetitions,1);
-            MAE_SVR_linear = zeros(numRepetitions,1);
-            r_SVR_linear = zeros(numRepetitions,1);
-            CI_SVR_linear = zeros(numRepetitions,2);
-            R2_SVR_linear = zeros(numRepetitions,1);
+            outputAllReps = struct();
+            avgOutput = struct();
+            
+            outputAllReps.MSE_SVR_linear = zeros(numRepetitions,1);
+            outputAllReps.MAE_SVR_linear = zeros(numRepetitions,1);
+            outputAllReps.r_SVR_linear = zeros(numRepetitions,1);
+            outputAllReps.CI_SVR_linear = zeros(numRepetitions,2);
+            outputAllReps.R2_SVR_linear = zeros(numRepetitions,1);
+            outputAllReps.behav_train_fit = cell(numRepetitions,1);
+            outputAllReps.inv_coef = cell(numRepetitions,1);
             
             for repIdx = 1:numRepetitions
                 outputThisRep = obj.execute(fcData,behavior,groupIds);   
-                MSE_SVR_linear(repIdx) = outputThisRep.MSE_SVR_linear;
-                MAE_SVR_linear(repIdx) = outputThisRep.MAE_SVR_linear;
-                r_SVR_linear(repIdx) = outputThisRep.r_SVR_linear;
-                CI_SVR_linear(repIdx,:) = outputThisRep.CI_SVR_linear;
-                R2_SVR_linear(repIdx) = outputThisRep.R2_SVR_linear;
+                outputAllReps.MSE_SVR_linear(repIdx) = outputThisRep.MSE_SVR_linear;
+                outputAllReps.MAE_SVR_linear(repIdx) = outputThisRep.MAE_SVR_linear;
+                outputAllReps.r_SVR_linear(repIdx) = outputThisRep.r_SVR_linear;
+                outputAllReps.CI_SVR_linear(repIdx,:) = outputThisRep.CI_SVR_linear;
+                outputAllReps.R2_SVR_linear(repIdx) = outputThisRep.R2_SVR_linear;
+                outputAllReps.behav_train_fit{repIdx} = outputThisRep.behav_train_fit;
+                outputAllReps.inv_coef{repIdx} = outputThisRep.inv_coef;
             end
             
-            outputAllReps.MSE_SVR_linear = MSE_SVR_linear;
-            outputAllReps.MAE_SVR_linear = MAE_SVR_linear;
-            outputAllReps.r_SVR_linear = r_SVR_linear;
-            outputAllReps.CI_SVR_linear = CI_SVR_linear;
-            outputAllReps.R2_SVR_linear = R2_SVR_linear;
-            
-            avgOutput.MSE_SVR_linear = mean(MSE_SVR_linear);
-            avgOutput.MAE_SVR_linear = mean(MAE_SVR_linear);
-            avgOutput.r_SVR_linear = mean(r_SVR_linear);
-            avgOutput.CI_SVR_linear = mean(CI_SVR_linear,1);
-            avgOutput.R2_SVR_linear = mean(R2_SVR_linear);
+            avgOutput.MSE_SVR_linear = mean(outputAllReps.MSE_SVR_linear);
+            avgOutput.MAE_SVR_linear = mean(outputAllReps.MAE_SVR_linear);
+            avgOutput.r_SVR_linear = mean(outputAllReps.r_SVR_linear);
+            avgOutput.CI_SVR_linear = mean(outputAllReps.CI_SVR_linear,1);
+            avgOutput.R2_SVR_linear = mean(outputAllReps.R2_SVR_linear);
             
             
         end
